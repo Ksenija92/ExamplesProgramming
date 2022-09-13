5ac41fef365faf4d46f50044f1550f6337b4a89c
Ксения <ksleonteva92@gmail.com>
2022-09-13 22:30:52 +0300

    Add Second Example Hello User

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Привет, мой дорогой друг");
+Console.WriteLine("Введи свое имя");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
